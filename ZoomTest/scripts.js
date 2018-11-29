35112bb1586d393bb1c7659dc37fac66898b638e
David Szucs <d@dfree.co.uk>
2018-11-29 11:10:18 +0100

    ES6 update with tween

--- ZoomTest/scripts.js
+++ ZoomTest/scripts.js
@@ -1,14 +1,11 @@
-var init = function () {
-    reset();
+let init =  () => {
+    document.getElementById('content').style.transform = 'matrix(0.2, 0, 0, 0.2, 0, 0)';
+    document.getElementById('largeImage').addEventListener('click', (e) => {
+        let div = document.getElementById('polygons').appendChild(document.createElement('div'));
+        div.style.transform = 'matrix(1, 0, 0, 1, '+(e.offsetX-25)+', '+(e.offsetY-25)+')';
+        div.classList.add('poly');
+        TweenMax.to('#content',1.2, {delay:0.4, scale:Math.random()*0.8+0.2, ease:Power3.easeInOut, transformOrigin:"0 0"});
+
+    });
 }
 
-function reset() {
-    document.getElementById('largeImage').addEventListener('click', clicked);
-}
-
-function clicked(e){
-    var div = document.createElement("div");
-    div.classList.add("poly");
-    div.style.transform = 'matrix(1, 0, 0, 1, '+(e.offsetX-50)+', '+(e.offsetY-50)+')';
-    var poly = document.getElementById('polygons').appendChild(div);
-}
