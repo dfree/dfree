abde496fdcb79b23cbbea0023c9951730eb62708
David Szucs <d@dfree.co.uk>
2019-02-11 23:42:46 +0100

    route update

---
+++ assets/com/precache-manifest.dc9a26a007f6ab9137d1ab4e24552b66.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "61c5806aa7d26085942bb88de3235ffe",
+    "url": "/assets/com/static/media/info_icon.61c5806a.svg"
+  },
+  {
+    "revision": "01aca00611d6d0cca3f1",
+    "url": "/assets/com/static/css/main.61c6f911.chunk.css"
+  },
+  {
+    "revision": "d04317e2254dbb6e353b",
+    "url": "/assets/com/static/js/runtime~main.d04317e2.js"
+  },
+  {
+    "revision": "d0d88f9d6e864435fb1e",
+    "url": "/assets/com/static/js/2.d0d88f9d.chunk.js"
+  },
+  {
+    "revision": "50659df547fcf6ec1e99ac0951cf3872",
+    "url": "/assets/com/static/media/brush_0.50659df5.svg"
+  },
+  {
+    "revision": "404d528902b969a80658ba6a9b88cbf0",
+    "url": "/assets/com/static/media/test_left.404d5289.png"
+  },
+  {
+    "revision": "565288ed82bbc4610833ab49a97da08e",
+    "url": "/assets/com/static/media/mouse_0.565288ed.svg"
+  },
+  {
+    "revision": "f6d9273ba1bb465897f018c216f37b94",
+    "url": "/assets/com/static/media/brush_1.f6d9273b.svg"
+  },
+  {
+    "revision": "9829f4a6e9592238bc7d05fa76e93b6e",
+    "url": "/assets/com/static/media/brush_2.9829f4a6.svg"
+  },
+  {
+    "revision": "5e6e9e518fa833a9360d9b43b4f84f9f",
+    "url": "/assets/com/static/media/brush_3.5e6e9e51.svg"
+  },
+  {
+    "revision": "5fd4370899501b01f903028e5cac8f6d",
+    "url": "/assets/com/static/media/brush_4.5fd43708.svg"
+  },
+  {
+    "revision": "301379598294f6b15d5ed72faef88176",
+    "url": "/assets/com/static/media/brush_5.30137959.svg"
+  },
+  {
+    "revision": "01aca00611d6d0cca3f1",
+    "url": "/assets/com/static/js/main.01aca006.chunk.js"
+  },
+  {
+    "revision": "31fca2155931d087ef5db24fe43c2ea0",
+    "url": "/assets/com/static/media/mouse_1.31fca215.svg"
+  },
+  {
+    "revision": "5320bb3f6ad34c91dc27a7375be1c38e",
+    "url": "/assets/com/static/media/mouse_2.5320bb3f.svg"
+  },
+  {
+    "revision": "4f56423b35a19ea54f0f7eafe1393ecd",
+    "url": "/assets/com/static/media/mouse_3.4f56423b.svg"
+  },
+  {
+    "revision": "54a093f47a0351e04c3e75ea65f98bcb",
+    "url": "/assets/com/static/media/mouse_4.54a093f4.svg"
+  },
+  {
+    "revision": "adf699a08d6ffbbdf95b9b750a002410",
+    "url": "/assets/com/static/media/mouse_5.adf699a0.svg"
+  },
+  {
+    "revision": "cc56ea72ecef853ef2e19246dbe4b3d5",
+    "url": "/assets/com/static/media/tool_polygon.cc56ea72.svg"
+  },
+  {
+    "revision": "50fc80800f42e7857f6d40147a30521d",
+    "url": "/assets/com/static/media/tool_bucket.50fc8080.svg"
+  },
+  {
+    "revision": "c95a38359b6c7659efcfe3a45ce66c76",
+    "url": "/assets/com/static/media/tool_brush.c95a3835.svg"
+  },
+  {
+    "revision": "1f356a75d4774148e99ab1d534840d71",
+    "url": "/assets/com/static/media/view_split.1f356a75.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/assets/com/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f051433e9051d6365565416e89378ebb",
+    "url": "/assets/com/index.html"
+  }
+];
--- assets/com/service-worker.js
+++ assets/com/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abedb19914b7f76485610375850f670d.js"
+  "/assets/com/precache-manifest.dc9a26a007f6ab9137d1ab4e24552b66.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/assets/com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
