90d661577f269631542886adbcb17885ab813dfd
David Szucs <d@dfree.co.uk>
2022-01-25 16:28:57 +0200

    new anim

--- common/next/js/headerAnim.js
+++ common/next/js/headerAnim.js
@@ -2,6 +2,9 @@
 var W = 0;
 var H = 0;
 
+var documentLoaded = false;
+var booksLoaded = false;
+
 // if letItDraw is true, it initiates a drawing sequence in tick()
 var letItDraw = false;
 var isTouchDevice = "ontouchstart" in window;
@@ -29,47 +32,101 @@ var shadowSrc = "img/shadow.png";
 //debug
 var printer = document.createElement("div");
 
+var imagesLoaded = 0;
+var bookPrototype = {
+  src: "img/book_1.jpg", // relative path to the book's image file 
+  startDelay: 0, // fade in delay
+  depth: 0, // book's virtual 3D distance from the page (0 - 0.8), ideally all books have different depths
+  originX: 0, // x center of the book
+  originY: 0, // y center of the book
+  originWidth: 0, // book width relative to the original background image size
+  originHeight: 0, // book height relative to the original background image size
+  x: 0, // book's rendered x top-left corner position
+  y: 0, // book's rendered y top-left corner position
+  width: 0, // book's rendered width
+  height: 0, // book's rendered height
+  img: null, // the actual loaded image
+  shadow: null, // shadow image
+  moveX: 0, // added drag position x
+  moveY: 0, // added drag position y
+  canvas: null, // book image rendered on a canvas in actual size
+  ctx: null, // book canvas context
+  shadowCanvas: null, // shadow image rendered on a canvas in actual size
+  shadowCtx: null, // shadow canvas context
+  shadowX: 0, // shadows's rendered x top-left corner position
+  shadowY: 0, // shadows's rendered y top-left corner position
+  shadowWidth: 0, // shadows's rendered width
+  shadowHeight: 0, // shadows's rendered height
+  offScreen: false, // if true, the book is not rendered anymore
+};
 // all sizes measured by placing the books on the original text image
-var books = [
+var bookSetup = [
   {
-    startDelay: 0.2,
-    depth: 0.2,
-    originX: 674, // center of the book
-    originY: 289, // center of the book
-    originWidth: 109,
-    originHeight: 151,
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-    src: "img/book_1.jpg",
-    img: null, // the actual loaded image
-    speedX: 0.1,
-    speedY: 0.05,
-    shadow: null,
-    moveX: 0,
-    moveY: 0,
+    src: "img/book_0.png",
+    startDelay: 0.8,
+    depth: 0.8,
+    originX: -171,
+    originY: 141,
+    originWidth: 185,
+    originHeight: 230,
   },
   {
-    startDelay: 0.4,
-    depth: 0.8,
-    originX: 163, // center of the book
-    originY: 419, // center of the book
-    originWidth: 145,
-    originHeight: 188,
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-    src: "img/book_2.jpg",
-    img: null, // the actual loaded image
-    speedX: 0.1,
-    speedY: -0.05,
-    shadow: null,
-    moveX: 0,
-    moveY: 0,
+    src: "img/book_1.png",
+    startDelay: 0.6,
+    depth: 0.6,
+    originX: -73,
+    originY: 527,
+    originWidth: 191,
+    originHeight: 227,
+  },
+  {
+    src: "img/book_2.png",
+    startDelay: 0.3,
+    depth: 0.3,
+    originX: 233,
+    originY: 375,
+    originWidth: 168,
+    originHeight: 227,
+  },
+  {
+    src: "img/book_3.png",
+    startDelay: 0.5,
+    depth: 0.4,
+    originX: 562,
+    originY: 14,
+    originWidth: 160,
+    originHeight: 216,
+  },
+  {
+    src: "img/book_4.png",
+    startDelay: 0.2,
+    depth: 0.7,
+    originX: 558,
+    originY: 440,
+    originWidth: 181,
+    originHeight: 254,
+  },
+  {
+    src: "img/book_5.png",
+    startDelay: 0.6,
+    depth: 0.2,
+    originX: 828,
+    originY: 579,
+    originWidth: 152,
+    originHeight: 230,
+  },
+  {
+    src: "img/book_6.png",
+    startDelay: 0.9,
+    depth: 0.82,
+    originX: 1038,
+    originY: 232,
+    originWidth: 241,
+    originHeight: 315,
   },
 ];
+
+var books = [];
 
 /*
 
@@ -78,20 +135,42 @@ var books = [
 */
 
 function initBeforeLoad() {
-  books.sort(function (a, b) {
+  bookSetup.sort(function (a, b) {
     if (a.depth < b.depth) return -1;
     if (a.depth > b.depth) return 1;
     return 0;
   });
+  for (var i = 0; i < bookSetup.length; i++) {
+    var newBook = Object.assign({}, bookPrototype);
+    Object.keys(bookSetup[i]).forEach(function(key){
+      newBook[key] = bookSetup[i][key];
+    });
+    books.push(newBook);
+  }
 
   for (var i = 0; i < books.length; i++) {
     var book = books[i];
     book.img = new Image();
-    book.img.style.opacity = 0;
+    book.img.onload = function () {
+      imagesLoaded++;
+      if (imagesLoaded == books.length) {
+        booksLoaded = true;
+        console.log("books loaded");
+        if (documentLoaded) {
+          startBooks();
+        }
+      }
+    };
     book.img.src = book.src;
     book.shadow = new Image();
     book.shadow.src = shadowSrc;
-    book.shadow.style.opacity = 0;
+    book.canvas = document.createElement("canvas");
+    book.canvas.style.opacity = 0;
+    book.canvas.imageSmoothingEnabled = true;
+    book.ctx = book.canvas.getContext("2d");
+    book.shadowCanvas = document.createElement("canvas");
+    book.shadowCanvas.style.opacity = 0;
+    book.shadowCtx = book.shadowCanvas.getContext("2d");
   }
 }
 initBeforeLoad();
@@ -107,10 +186,10 @@ function initOnLoad() {
       width: headerTextBounds.width,
       height: headerTextBounds.height,
     };
-    initBooks();
     resize();
+    initBooks();
     printer.style = "position: fixed; top: 10px; left: 10px; font-size: 20px;";
-    document.body.appendChild(printer);
+    // document.body.appendChild(printer);
     printer.innerHTML = "0 : 0 : 0";
 
     window.addEventListener("resize", resize);
@@ -154,23 +233,28 @@ function touchEnd() {
   }
 }
 function handleOrientation(event) {
-  var keys = "";
   gyroEventAdded = true;
-  //Object.keys(event).forEach((key) => keys += key+', ');
   printer.innerHTML =
     "gyroscope <br/>x: " +
     Math.round(event.gamma) +
     "<br/>y: " +
     Math.round(event.beta) +
     "<br/>alpha:" +
-    Math.round(event.alpha)
-
-  mousePos = {
-    x: ((event.gamma + 90) / 180) * W,
-    y: ((event.beta + 90) / 180) * H,
-  };
+    Math.round(event.alpha);
+  if (
+    event.gamma > -90 &&
+    event.gamma < 90 &&
+    event.beta < 70 &&
+    event.beta > -70
+  ) {
+    mousePos = {
+      x: ((event.gamma + 90) / 180) * W,
+      y: ((event.beta + 90) / 180) * H,
+    };
+  }
   letItDraw = true;
 }
+
 window.addEventListener("load", initOnLoad);
 
 function resize() {
@@ -212,11 +296,10 @@ function resize() {
 
   lightOrigin = { x: W / 2, y: H / 2 };
   light = { x: lightOrigin.x, y: lightOrigin.y };
-  /* if (!light) {
-    light = { x: lightOrigin.x, y: lightOrigin.y };
-  } */
 
-  resizeBooks();
+  if (documentLoaded && booksLoaded) {
+    resizeBooks();
+  }
 
   frameSkipper = false;
   letItDraw = true;
@@ -245,14 +328,14 @@ function onMouseDown(e) {
 function onMouseUp(e) {
   activeBook = -1;
 }
+
 function onMouseLeave(e) {
   activeBook = -1;
   prevCursor = null;
   onMouseMove({ pageX: W / 2, pageY: H / 2, screenX: W / 2, screenY: H / 2 });
 }
+
 function onMouseMove(e) {
-  //light = { x: e.screenX, y: e.screenY };
-  //mousePos = { x: e.screenX, y: e.screenY };
   if (!prevCursor) {
     prevCursor = { x: e.pageX, y: e.pageY };
   } else {
@@ -295,34 +378,40 @@ function initBooks() {
   bookContainer.style =
     "position: fixed; left: 0; top:0; pointer-events: none;";
   document.body.appendChild(bookContainer);
+  documentLoaded = true;
 
+  console.log("documentLoaded", booksLoaded);
+  if (booksLoaded) {
+    startBooks();
+  }
+}
+
+function startBooks() {
+  console.log("startBooks");
+  resizeBooks();
   var baseFadeDelay = 0.4;
-
   for (var i = 0; i < books.length; i++) {
     var book = books[i];
-    book.shadow.style.position = "absolute";
-    bookContainer.appendChild(book.shadow);
-    gsap.to(book.shadow, {
+    book.shadowCanvas.style.position = "absolute";
+    bookContainer.appendChild(book.shadowCanvas);
+    gsap.to(book.shadowCanvas, {
       duration: 1,
-      opacity: (1 - book.depth) * 0.5,
+      opacity: 1,
       delay: baseFadeDelay + book.startDelay,
       ease: "power2.inOut",
     });
 
-    book.img.style.position = "absolute";
-    bookContainer.appendChild(book.img);
-    gsap.to(book.img, {
+    book.canvas.style.position = "absolute";
+    bookContainer.appendChild(book.canvas);
+    gsap.to(book.canvas, {
       duration: 1,
       opacity: 1,
       delay: baseFadeDelay + book.startDelay,
       ease: "power2.inOut",
     });
   }
-
   tick();
-  // moveBooks();
 }
-
 function tick() {
   if (frameSkipper) {
     frameSkipper = false;
@@ -334,6 +423,31 @@ function tick() {
     }
   }
   requestAnimationFrame(tick);
+}
+
+function isBookOnScreen(book) {
+  var bookOff = false;
+  var shadowOff = false;
+  if (
+    book.x + book.width < 0 ||
+    book.y + book.height < 0 ||
+    book.x > W ||
+    book.y > H
+  ) {
+    bookOff = true;
+  }
+  if (
+    book.shadowX + book.shadowWidth < 0 ||
+    book.shadowY + book.shadowHeight < 0 ||
+    book.shadowX > W ||
+    book.shadowY > H
+  ) {
+    shadowOff = true;
+  }
+  if (bookOff && shadowOff) {
+    return false;
+  }
+  return true;
 }
 
 function renderBooks() {
@@ -365,31 +479,41 @@ function renderBooks() {
         (1 + book.depth) -
       scrollY * (1 + book.depth / 2) +
       book.moveY;
-    //var bookCenterX = book.originX * scale + headerProjection.x;
-    //var bookCenterY = book.originY * scale + headerProjection.y - (scrollY  * (1 + book.depth / 4));
+
     book.x = bookCenterX - book.width / 2;
     book.y = bookCenterY - book.height / 2;
-    book.img.style.transform = "translate(" + book.x + "px, " + book.y + "px)";
 
     var lightDistance = {
       x: bookCenterX - light.x,
       y: bookCenterY - light.y,
     };
 
-    var shadow = book.shadow;
-    var shadowWidth = book.width * (1.05 + book.depth / 6);
-    var shadowHeight = book.height * (1.05 + book.depth / 6);
-    var shadowX =
+    book.shadowX =
       bookCenterX -
-      shadowWidth / 2 +
+      book.shadowWidth / 2 +
       (lightDistance.x / W) * (headerProjection.width / 60) * multiplyBookDepth;
-    var shadowY =
+    book.shadowY =
       bookCenterY -
-      shadowHeight / 2 +
+      book.shadowHeight / 2 +
       (lightDistance.y / W) * (headerProjection.width / 60) * multiplyBookDepth;
-    shadow.style.width = shadowWidth + "px";
-    shadow.style.height = shadowHeight + "px";
-    shadow.style.transform = "translate(" + shadowX + "px, " + shadowY + "px)";
+
+    if (isBookOnScreen(book)) {
+      if (book.offScreen) {
+        book.offScreen = false;
+        book.canvas.style.visibility = "visible";
+        book.shadowCanvas.style.visibility = "visible";
+      }
+      book.canvas.style.transform =
+        "translate(" + book.x + "px, " + book.y + "px)";
+      book.shadowCanvas.style.transform =
+        "translate(" + book.shadowX + "px, " + book.shadowY + "px)";
+    } else {
+      if (!book.offScreen) {
+        book.offScreen = true;
+        book.canvas.style.visibility = "hidden";
+        book.shadowCanvas.style.visibility = "hidden";
+      }
+    }
   }
 }
 
@@ -399,17 +523,25 @@ function resizeBooks() {
     var book = books[i];
     book.width = book.originWidth * scale;
     book.height = book.originHeight * scale;
-    book.img.style.width = book.width + "px";
-    book.img.style.height = book.height + "px";
-  }
-}
 
-function moveBooks() {
-  for (var i = 0; i < books.length; i++) {
-    var book = books[i];
-    book.originX += book.speedX;
-    book.originY += book.speedY;
+    book.canvas.width = book.width * 2;
+    book.canvas.height = book.height * 2;
+    book.canvas.style.width = book.width + "px";
+    book.canvas.style.height = book.height + "px";
+    book.ctx.drawImage(book.img, 0, 0, book.width * 2, book.height * 2);
+
+    book.shadowOpacity = (1 - book.depth) * 0.5;
+    book.shadowWidth = book.width * (1.05 + book.depth / 6);
+    book.shadowHeight = book.height * (1.05 + book.depth / 6);
+    book.shadowCanvas.width = book.shadowWidth;
+    book.shadowCanvas.height = book.shadowHeight;
+    book.shadowCtx.globalAlpha = book.shadowOpacity;
+    book.shadowCtx.drawImage(
+      book.shadow,
+      0,
+      0,
+      book.shadowWidth,
+      book.shadowHeight
+    );
   }
-  letItDraw = true;
-  requestAnimationFrame(moveBooks);
 }
